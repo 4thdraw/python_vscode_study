2c9df29cb3bf86ed756fb377f7624c78c8fb0e08
BoYeungBang <57360625+4thdraw@users.noreply.github.com>
2026-03-03 07:09:44 +0000

    데이터의 구조분석

--- pandas/2주차공부.py
+++ pandas/2주차공부.py
@@ -42,4 +42,42 @@ print(df_t.columns)
 # %%
 df_t = df_t[1:]
 print(df_t.head())
+
+# %%
+
+import numpy as np
+df_t[['신고수', '신축수']] = df_t[['신고수', '신축수']].replace(0, np.nan)
+print(df_t)
+
+# %%
+print(df_t.isnull().sum())
+# %%
+# '신고수'와 '신축수' 컬럼의 빈값을 숫자 0으로 채우기
+df_t[['신고수', '신축수']] = df_t[['신고수', '신축수']].fillna(0)
+print('빈값은 \n',df_t.isnull().sum())
+
+print('0의 값은 \n', (df_t == 0).sum())
+# %%
+# 1. 각 행의 모든 값이 0인지 검사 (True/False 결과 반환)
+# axis=0 (기본값): 세로 방향 (위에서 아래로)
+
+#axis=1: 가로 방향 (왼쪽에서 오른쪽으로)
+# null은 실수라 다른 값으로 대체될때 소수점이 생김
+print(df_t)
+all_zero_rows = (df_t == 0).all(axis=1)
+
+# 2. True(모든 값이 0인 행)의 개수 합산
+print(all_zero_rows.sum())
+# %%
+df_t[['신고수', '신축수']] = df_t[['신고수', '신축수']].astype(int)
+print(df_t.head(3))
+all_zero_rows = (df_t == 0).all(axis=1)
+
+# 2. True(모든 값이 0인 행)의 개수 합산
+print('모든 행의 값이 0인 행의 수는 ', all_zero_rows.sum())
+# %%
+custom_zero_rows = (df_t[['신고수', '신축수']] == 0).all(axis=1)
+count = custom_zero_rows.sum()
+print(f"두 컬럼 모두 0인 행의 개수: {count}")
+print('전체 행의 개수 ',df_t.shape[0])
 # %%
